42d8c94da0ff04ead273aa5ca8c1df20e8102907
bhujelvictor <bhujelvictor@gmail.com>
2024-01-03 15:40:26 +0545

    Added api/ endpoint to project url conf

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from accounts.views import *
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register-user'),
-    path('login/', UserLoginView.as_view(), name='login-user'),
-    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
-    path('resend-code/', ResendVerificationCodeView.as_view(), name='resend-code')
+    path('api/register/', UserRegistrationView.as_view(), name='register-user'),
+    path('api/login/', UserLoginView.as_view(), name='login-user'),
+    path('api/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
+    path('api/resend-code/', ResendVerificationCodeView.as_view(), name='resend-code')
 ]
 
